f72959bf6373ec3b06febe7d01b50a5a06242ba3
elviswlima <elvis.wlima@hotmail.com>
2022-04-16 00:28:31 -0300

    criei as classes, atributos e métodos

--- src/main/java/br/com/edg/project/model/Caixa.java
+++ src/main/java/br/com/edg/project/model/Caixa.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package br.com.edg.project.model;
 
 /**
@@ -9,5 +5,55 @@ package br.com.edg.project.model;
  * @author Danilo
  */
 public class Caixa {
+    
+    private int codProduto;
+    private String cpfCliente;
+    private Double qtde;
+    private String formaPagamento;
+    private Double valorTotal;
+
+    public Caixa() {
+    }
+
+    public int getCodProduto() {
+        return codProduto;
+    }
+
+    public void setCodProduto(int codProduto) {
+        this.codProduto = codProduto;
+    }
+
+    public String getCpfCliente() {
+        return cpfCliente;
+    }
+
+    public void setCpfCliente(String cpfCliente) {
+        this.cpfCliente = cpfCliente;
+    }
+
+    public Double getQtde() {
+        return qtde;
+    }
+
+    public void setQtde(Double qtde) {
+        this.qtde = qtde;
+    } 
+    
+    public String getFormaPagamento() {
+        return formaPagamento;
+    }
+
+    public void setFormaPagamento(String formaPagamento) {
+        this.formaPagamento = formaPagamento;
+    }
+
+    public Double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(Double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+    
     
 }
--- src/main/java/br/com/edg/project/model/Estoque.java
+++ src/main/java/br/com/edg/project/model/Estoque.java
@@ -9,5 +9,45 @@ package br.com.edg.project.model;
  * @author Danilo
  */
 public class Estoque {
+    private int codProduto;
+    private String nomeProduto;
+    private int qtdeProduto;
+    private String validade;
+    
+    public Estoque() {
+    }
+
+    public int getCodProduto() {
+        return codProduto;
+    }
+
+    public void setCodProduto(int codProduto) {
+        this.codProduto = codProduto;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public int getQtdeProduto() {
+        return qtdeProduto;
+    }
+
+    public void setQtdeProduto(int qtdeProduto) {
+        this.qtdeProduto = qtdeProduto;
+    }
+
+    public String getValidade() {
+        return validade;
+    }
+
+    public void setValidade(String validade) {
+        this.validade = validade;
+    }
+    
     
 }
