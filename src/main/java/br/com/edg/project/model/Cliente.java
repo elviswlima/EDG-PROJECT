342182fdae8269153e96a84d65d0d8b7d1a609d6
Danilo Almeida <48030619+DaniloAlmeidaSantos@users.noreply.github.com>
2022-04-16 23:41:49 -0300

    Revert "criei as classes, atributos e métodos"
    This reverts commit f72959bf6373ec3b06febe7d01b50a5a06242ba3.

--- src/main/java/br/com/edg/project/model/Cliente.java
+++ src/main/java/br/com/edg/project/model/Cliente.java
@@ -10,14 +10,12 @@ package br.com.edg.project.model;
  * @author elvis.wlima
  */
 public class Cliente {
-    
     private String nome;
     private String cpf;
     private String telefone;
     private String dataNascimento;
     private String sexo;
     private String rg;
-    private String email;
     private String cep;
     private String cidade;
     private String bairro;
@@ -85,14 +83,6 @@ public class Cliente {
         this.rg = rg;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    
     public String getCep() {
         return cep;
     }
--- src/main/java/br/com/edg/project/model/Produto.java
+++ src/main/java/br/com/edg/project/model/Produto.java
@@ -1,3 +1,7 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package br.com.edg.project.model;
 
 /**
@@ -5,48 +9,5 @@ package br.com.edg.project.model;
  * @author Danilo
  */
 public class Produto {
-    
-    private String nomeProduto;
-    private int codProduto;
-    private double valorProduto;
-    private double qtdeProduto;
-    private double qtdePorKg;
-    
-    public Produto() {
-    }
-
-    public String getNomeProduto() {
-        return nomeProduto;
-    }
-
-    public void setNomeProduto(String nomeProduto) {
-        this.nomeProduto = nomeProduto;
-    }
-
-    public int getCodProduto() {
-        return codProduto;
-    }
-
-    public void setCodProduto(int codProduto) {
-        this.codProduto = codProduto;
-    }
-
-    public double getValorProduto() {
-        return valorProduto;
-    }
-
-    public void setValorProduto(double valorProduto) {
-        this.valorProduto = valorProduto;
-    }
-
-    public double getQtdeProduto() {
-        return qtdeProduto;
-    }
-
-    public void setQtdeProduto(double qtdeProduto) {
-        this.qtdeProduto = qtdeProduto;
-    }
-    
-    
     
 }
