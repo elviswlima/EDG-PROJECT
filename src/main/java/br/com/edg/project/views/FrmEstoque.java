8a94f5c6a84d6aa1d897ca639e6195285daac792
elviswlima <elvis.wlima@hotmail.com>
2022-05-13 11:15:51 -0300

    tabela editável

--- src/main/java/br/com/edg/project/views/FrmEstoque.java
+++ src/main/java/br/com/edg/project/views/FrmEstoque.java
@@ -66,9 +66,16 @@ public class FrmEstoque extends javax.swing.JFrame {
             Class[] types = new Class [] {
                 java.lang.Object.class, java.lang.String.class, java.lang.Integer.class, java.lang.String.class
             };
+            boolean[] canEdit = new boolean [] {
+                false, true, true, true
+            };
 
             public Class getColumnClass(int columnIndex) {
                 return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
             }
         });
         jScrollPane1.setViewportView(tblEstoque);
